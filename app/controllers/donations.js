6bd68cbfbc3abb8d5a4d9fa3721243bc8e0bb076
murphy mj <2martinmurphy@gmail.com>
2019-03-24 14:40:53 +0000

    Fist commit

---
+++ app/controllers/donations.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const User = require('../models/user');
+const Donation = require('../models/donation');
+const Candidate = require('../models/candidate');
+
+const Donations = {
+  home: {
+    handler: async function(request, h) {
+      const candidates = await Candidate.find();
+      return h.view('home', { title: 'Make a Donation', candidates: candidates });
+    }
+  },
+  report: {
+    handler: async function(request, h) {
+      try {
+        const donations = await Donation.find().populate('donor').populate('candidate');
+        return h.view('report', {
+          title: 'Donations to Date',
+          donations: donations
+        });
+      } catch (err) {
+        return h.view('main', { errors: [{ message: err.message }] });
+      }
+    }
+  },
+  donate: {
+    handler: async function(request, h) {
+      try {
+        const id = request.auth.credentials.id;
+        const user = await User.findById(id);
+        const data = request.payload;
+
+        const rawCandidate = request.payload.candidate.split(',');
+        const candidate = await Candidate.findOne({
+          lastName: rawCandidate[0],
+          firstName: rawCandidate[1]
+        });
+
+        const newDonation = new Donation({
+          amount: data.amount,
+          method: data.method,
+          donor: user._id,
+          candidate: candidate._id
+        });
+        await newDonation.save();
+        return h.redirect('/report');
+      } catch (err) {
+        return h.view('main', { errors: [{ message: err.message }] });
+      }
+    }
+  }
+};
+
+module.exports = Donations;
---
+++ test/candidatesapitest.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const assert = require('chai').assert;
+const DonationService = require('./donation-service');
+const fixtures = require('./fixtures.json');
+const _ = require('lodash');
+require('mocha').suite;
+
+suite('Candidate API tests', function () {
+
+  let candidates = fixtures.candidates;
+  let newCandidate = fixtures.newCandidate;
+
+  const donationService = new DonationService('http://localhost:3000');
+
+
+  setup(async function () {
+    await donationService.deleteAllCandidates();
+  });
+
+  teardown(async function () {
+    await donationService.deleteAllCandidates();
+  });
+
+
+
+  // test('create a candidate', async function () {
+ //   const returnedCandidate = await donationService.createCandidate(newCandidate);
+  //  assert.equal(returnedCandidate.firstName, newCandidate.firstName);
+  //  assert.equal(returnedCandidate.lastName, newCandidate.lastName);
+ //   assert.equal(returnedCandidate.office, newCandidate.office);
+//    assert.isDefined(returnedCandidate._id);
+//  });
+
+
+//  test('create a candidate', async function () {
+ //   const returnedCandidate = await donationService.createCandidate(newCandidate);
+  //  assert.equal(returnedCandidate, newCandidate);
+ //   assert.isDefined(returnedCandidate._id);
+//  });
+
+  test('create a candidate', async function () {
+    const returnedCandidate = await donationService.createCandidate(newCandidate);
+    assert(_.some([returnedCandidate], newCandidate),  'returnedCandidate must be a superset of newCandidate');
+    assert.isDefined(returnedCandidate._id);
+  })
+
+  test('delete a candidate', async function () {
+    let c = await donationService.createCandidate(newCandidate);
+    assert(c._id != null);
+    await donationService.deleteOneCandidate(c._id);
+    c = await donationService.getCandidate(c._id);
+    assert(c == null);
+  });
+
+  test('get candidate', async function () {
+    const c1 = await donationService.createCandidate(newCandidate);
+    const c2 = await donationService.getCandidate(c1._id);
+    assert.deepEqual(c1, c2);
+  });
+
+  test('get invalid candidate', async function () {
+    const c1 = await donationService.getCandidate('1234');
+    assert.isNull(c1);
+    const c2 = await donationService.getCandidate('012345678901234567890123');
+    assert.isNull(c2);
+  });
+
+  test('delete a candidate', async function () {
+    let c = await donationService.createCandidate(newCandidate);
+    assert(c._id != null);
+    await donationService.deleteOneCandidate(c._id);
+    c = await donationService.getCandidate(c._id);
+    assert(c == null);
+  });
+
+  test('get all candidates', async function () {
+    for (let c of candidates) {
+      await donationService.createCandidate(c);
+    }
+
+    const allCandidates = await donationService.getCandidates();
+    assert.equal(allCandidates.length, candidates.length);
+  });
+
+  test('get candidates detail', async function () {
+    for (let c of candidates) {
+      await donationService.createCandidate(c);
+    }
+    const allCandidates = await donationService.getCandidates();
+    for (var i = 0; i < candidates.length; i++) {
+      assert(_.some([allCandidates[i]], candidates[i]), 'returnedCandidate must be a superset of newCandidate');
+    }
+  });
+
+  test('get all candidates empty', async function () {
+    const allCandidates = await donationService.getCandidates();
+    assert.equal(allCandidates.length, 0);
+  });
+
+
+
+
+});
+
